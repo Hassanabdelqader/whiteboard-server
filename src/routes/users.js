bae834995cfd7c485a44fda07e0deb7a8073c2c5
Hassanabdelqader <terawihassan@gmail.com>
2022-09-26 18:58:13 +0000

    v1

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,7 @@ const signin = require("../middlewares/users/AuthSignin");
 const signup = require("../middlewares/users/AuthSignup");
 const addAdmin = require("../middlewares/users/AuthSignup/authaddAdimn");
 const isBlocked = require("../middlewares/users/checkBlackList");
+const editAdmin = require("../middlewares/users/EditAdmin");
 const findUser = require("../middlewares/users/FindUser");
 const isAdmin = require("../middlewares/users/IsAdmin");
 const resetpassword = require("../middlewares/users/ResetPassword");
@@ -32,6 +33,11 @@ router.post("/signup", signup, (req, res) => {
 router.post("/addAdmin", addAdmin, (req, res) => {
   res.status(200).send(req.users);
 });
+
+router.put("/editAdmin/:id", auth,editAdmin , (req, res) => {
+  res.status(200).send(req.updateduser);
+});
+
 
 router.post("/signin", signin, (req, res) => {
   res.status(200).send(req.users);
